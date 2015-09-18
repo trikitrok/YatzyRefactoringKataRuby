a11501d3f4391c8e54bfdfbe7e46697f9744ee67
trikitrok <trikitrok@gmail.com>
2015-09-18 17:46:19 +0200

    Refactor: using reduce in chance

--- yatzy.rb
+++ yatzy.rb
@@ -1,12 +1,6 @@
 class Yatzy
-  def self.chance(d1, d2, d3, d4, d5)
-    total = 0
-    total += d1
-    total += d2
-    total += d3
-    total += d4
-    total += d5
-    return total
+  def self.chance(*dies)
+    dies.reduce(:+)
   end
 
   def self.yatzy(dice)
