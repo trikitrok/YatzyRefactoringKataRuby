f379451360ada1bdbbe9d4b58a78b67525189952
trikitrok <trikitrok@gmail.com>
2015-09-20 00:35:25 +0200

    Refactor: renamed some tests

--- test_yatzy.rb
+++ test_yatzy.rb
@@ -34,7 +34,7 @@ class YatzyTest < Test::Unit::TestCase
     assert 12 == Yatzy.threes(2,3,3,3,3)
   end
 
-  def test_fours_test
+  def test_fours
     assert 12 == Yatzy.fours(4,4,4,5,5)
     assert 12 == Yatzy.fours(4,4,4,5,5)
     assert 8 == Yatzy.fours(4,4,5,5,5)
@@ -47,7 +47,7 @@ class YatzyTest < Test::Unit::TestCase
     assert 20 == Yatzy.fives(4,5,5,5,5)
   end
 
-  def test_sixes_test
+  def test_sixes
     assert 0 == Yatzy.sixes(4,4,4,5,5)
     assert 6 == Yatzy.sixes(4,4,6,5,5)
     assert 18 == Yatzy.sixes(6,5,6,6,5)
@@ -59,7 +59,7 @@ class YatzyTest < Test::Unit::TestCase
     assert 12 == Yatzy.score_pair(5,3,6,6,5)
   end
 
-  def test_two_Pair
+  def test_two_pairs
     assert_equal 16, Yatzy.two_pair(3,3,5,4,5)
     assert_equal 16, Yatzy.two_pair(3,3,5,5,5)
   end
@@ -77,19 +77,19 @@ class YatzyTest < Test::Unit::TestCase
     assert 12 == Yatzy.four_of_a_kind(3,3,3,3,3)
   end
 
-  def test_smallStraight
+  def test_small_straight
     assert 15 == Yatzy.small_straight(1,2,3,4,5)
     assert 15 == Yatzy.small_straight(2,3,4,5,1)
     assert 0 == Yatzy.small_straight(1,2,2,4,5)
   end
 
-  def test_largeStraight
+  def test_large_straight
     assert 20 == Yatzy.large_straight(6,2,3,4,5)
     assert 20 == Yatzy.large_straight(2,3,4,5,6)
     assert 0 == Yatzy.large_straight(1,2,2,4,5)
   end
 
-  def test_fullHouse
+  def test_full_house
     assert 18 == Yatzy.full_house(6,2,2,2,6)
     assert 0 == Yatzy.full_house(2,3,4,5,6)
   end
