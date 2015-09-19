ae9a9aed78064a01f9203c7927fa062579e57559
trikitrok <trikitrok@gmail.com>
2015-09-19 23:48:13 +0200

    Refactor: Using frequencies helper to simplify two_pair

--- yatzy.rb
+++ yatzy.rb
@@ -49,26 +49,9 @@ class Yatzy
     (pairs.keys.max || 0) * 2
   end
 
-  def self.two_pair( d1,  d2,  d3,  d4,  d5)
-    counts = [0]*6
-    counts[d1-1] += 1
-    counts[d2-1] += 1
-    counts[d3-1] += 1
-    counts[d4-1] += 1
-    counts[d5-1] += 1
-    n = 0
-    score = 0
-    for i in Array 0..5
-      if (counts[6-i-1] >= 2)
-        n = n+1
-        score += (6-i)
-      end
-    end
-    if (n == 2)
-      return score * 2
-    else
-      return 0
-    end
+  def self.two_pair(*dies)
+    pairs = extract_group(dies, 2)
+    pairs.keys.reduce(:+) * 2
   end
 
   def self.four_of_a_kind( _1,  _2,  d3,  d4,  d5)
